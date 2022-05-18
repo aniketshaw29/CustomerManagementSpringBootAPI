c8b218cfacdbc5cbd8cfdbc2cd37e4da38824819
Aniket Shaw <aniketshawcoding@gmail.com>
2022-05-18 12:40:13 +0530

    cross origin

--- src/main/java/com/project/customermanagement/CustomermanagementApplication.java
+++ src/main/java/com/project/customermanagement/CustomermanagementApplication.java
@@ -2,6 +2,11 @@ package com.project.customermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CustomermanagementApplication {
@@ -9,5 +14,19 @@ public class CustomermanagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomermanagementApplication.class, args);
 	}
+	@Configuration
+    public class CorsConfig {
 
+        @SuppressWarnings("deprecation")
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurerAdapter() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("")
+                            .allowedHeaders("*");
+                }
+            };
+        }
+    }
 }
